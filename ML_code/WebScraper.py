52e63c5470f2788b3321900de185b1049aa5def9
Utkarsh Chirimar <uclego2001@gmail.com>
2017-08-03 12:07:47 -0400

    scraps the web

--- ML_code/WebScraper.py
+++ ML_code/WebScraper.py
@@ -6,9 +6,17 @@ outfile = open('data_output.csv', 'w')
 writer = csv.writer(outfile)
 
 soup = BeautifulSoup(urllib.request.urlopen("http://nvbdcp.gov.in/malaria3.html").read(), 'lxml')
-
 table = soup("table", {"width":"618"})[0].find_all('tr')
+for row in table:
+    cols = row.findChildren(recursive=False)
+    cols = [ele.text.strip() for ele in cols]
+    writer.writerow(cols)
+    print(cols)
 
+writer.writerow('\n')
+
+soup = BeautifulSoup(urllib.request.urlopen("https://malariajournal.biomedcentral.com/articles/10.1186/1475-2875-13-126").read(), 'lxml')
+table = soup("table", {"xmlns":"http://www.w3.org/1999/xhtml"})[0].find_all('tr')
 for row in table:
     cols = row.findChildren(recursive=False)
     cols = [ele.text.strip() for ele in cols]
